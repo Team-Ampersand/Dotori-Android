4788595e18edf31a1a6e3b67798c6c78b588d543
박성현 <pjhomg5530@naver.com>
2023-09-05 09:21:35 +0900

    :green_heart: :: ktlint 의존성 추가

--- buildSrc/src/main/java/ProjectProperties.kt
+++ buildSrc/src/main/java/ProjectProperties.kt
@@ -3,6 +3,7 @@ object ProjectProperties {
         const val ANDROID_APPLICATION = "com.android.application"
         const val ANDROID_LIBRARY = "com.android.library"
         const val KOTLIN_ANDROID = "org.jetbrains.kotlin.android"
+        const val KTLINT = "org.jlleitschuh.gradle.ktlint"
     }
 
     object Versions {
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,6 +1,7 @@
 object Versions {
     const val GRADLE_ANDROID = "7.3.1"
     const val GRADLE_KOTLIN = "1.7.20"
+    const val GRADLE_KTLINT = "11.3.2"
 
     const val CORE_KTX = "1.9.0"
     const val APP_COMPAT = "1.6.1"
