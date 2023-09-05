abff5e31e2002622ff201c81bbd615312151fcb4
박성현 <pjhomg5530@naver.com>
2023-09-05 09:40:06 +0900

    :green_heart: :: 공백 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id(ProjectProperties.GradlePlugin.ANDROID_APPLICATION) version Versions.GRADLE_ANDROID apply false
-    id(ProjectProperties.GradlePlugin.ANDROID_LIBRARY) version Versions.GRADLE_ANDROID apply  false
+    id(ProjectProperties.GradlePlugin.ANDROID_LIBRARY) version Versions.GRADLE_ANDROID apply false
     id(ProjectProperties.GradlePlugin.KOTLIN_ANDROID) version Versions.GRADLE_KOTLIN apply false
     id(ProjectProperties.GradlePlugin.KTLINT) version Versions.GRADLE_KTLINT
 }
