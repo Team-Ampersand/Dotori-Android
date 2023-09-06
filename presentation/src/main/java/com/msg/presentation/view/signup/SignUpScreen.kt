df82ebbd240619d0595643a90a445d919438b516
박성현 <pjhomg5530@naver.com>
2023-09-06 15:58:11 +0900

    :lipstick: :: SignUpScreen 구현

---
+++ presentation/src/main/java/com/msg/presentation/view/signup/SignUpScreen.kt
@@ -0,0 +1,39 @@
+package com.msg.presentation.view.signup
+
+import android.annotation.SuppressLint
+import android.net.http.SslError
+import android.view.ViewGroup
+import android.webkit.SslErrorHandler
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun SignUpScreen() {
+    val url = "https://www.dotori-gsm.com/signup"
+
+    AndroidView(factory = {
+        WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            webViewClient = object : WebViewClient() {
+                @SuppressLint("WebViewClientOnReceivedSslError")
+                override fun onReceivedSslError(
+                    view: WebView?,
+                    handler: SslErrorHandler?,
+                    error: SslError?
+                ) {
+                    super.onReceivedSslError(view, handler, error)
+                    handler!!.proceed()
+                }
+            }
+            loadUrl(url)
+        }
+    })
+}
