672b482fa300ce3dfdf9faa3ce5d51dfdff98e32
박성현 <pjhomg5530@naver.com>
2023-09-07 16:03:01 +0900

    :sparkles: :: safeApiCall 적용

--- data/src/main/java/com/msg/data/remote/datasource/auth/AuthDataSourceImpl.kt
+++ data/src/main/java/com/msg/data/remote/datasource/auth/AuthDataSourceImpl.kt
@@ -3,12 +3,13 @@ package com.msg.data.remote.datasource.auth
 import com.msg.data.remote.dto.auth.LoginRequest
 import com.msg.data.remote.dto.auth.LoginResponse
 import com.msg.data.remote.network.AuthApi
+import com.msg.data.remote.util.safeApiCall
 import javax.inject.Inject
 
 class AuthDataSourceImpl @Inject constructor(
     private val authApi: AuthApi
 ): AuthDataSource {
-    override suspend fun login(loginRequest: LoginRequest): LoginResponse = authApi.login(loginRequest)
+    override suspend fun login(loginRequest: LoginRequest): LoginResponse = safeApiCall { authApi.login(loginRequest) }
 
-    override suspend fun tokenReissue(): LoginResponse = authApi.tokenReissue()
+    override suspend fun tokenReissue(): LoginResponse = safeApiCall { authApi.tokenReissue() }
 }
