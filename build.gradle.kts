8b53d4d215582089dc5ace6bf634746f9aaa03fd
박성현 <pjhomg5530@naver.com>
2023-09-07 11:01:37 +0900

    :heavy_plus_sign: :: HILT_PLUGIN 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id(ProjectProperties.GradlePlugin.ANDROID_LIBRARY) version Versions.GRADLE_ANDROID apply false
     id(ProjectProperties.GradlePlugin.KOTLIN_ANDROID) version Versions.GRADLE_KOTLIN apply false
     id(ProjectProperties.GradlePlugin.KTLINT) version Versions.GRADLE_KTLINT
+    id(ProjectProperties.GradlePlugin.HILT_PLUGIN) version Versions.HILT apply false
 }
