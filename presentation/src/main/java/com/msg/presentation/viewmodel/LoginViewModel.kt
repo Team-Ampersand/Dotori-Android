42ac3f2e02cd32d1015d2ae8ab550abda1b61316
박성현 <pjhomg5530@naver.com>
2023-09-08 14:38:11 +0900

    :sparkles: :: Flow 구현

--- presentation/src/main/java/com/msg/presentation/viewmodel/LoginViewModel.kt
+++ presentation/src/main/java/com/msg/presentation/viewmodel/LoginViewModel.kt
@@ -3,9 +3,13 @@ package com.msg.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.msg.domain.model.auth.LoginRequestModel
+import com.msg.domain.model.auth.LoginResponseModel
 import com.msg.domain.usecase.auth.LoginUseCase
 import com.msg.domain.usecase.auth.SaveTokenUseCase
 import com.msg.presentation.exception.exceptionHandling
+import com.msg.presentation.viewmodel.util.Event
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,10 +17,15 @@ class LoginViewModel @Inject constructor(
     private val loginUseCase: LoginUseCase,
     private val saveTokenUseCase: SaveTokenUseCase
 ): ViewModel() {
+
+    private val _loginState = MutableStateFlow<Event<LoginResponseModel>>(Event.Loading)
+    var loginState = _loginState.asStateFlow()
+
     fun login(loginRequestModel: LoginRequestModel) {
         viewModelScope.launch {
             loginUseCase(loginRequestModel)
                 .onSuccess {
+                    _loginState.value = Event.Success(it)
                     saveTokenUseCase(
                         accessToken = it.accessToken,
                         refreshToken = it.refreshToken,
