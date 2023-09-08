c5a65085124e8b10e7ab028acaa65069e5beeeaf
hyeonseo <s21035@gsm.hs.kr>
2023-09-08 12:22:02 +0900

    :sparkles: :: Calendar & Option BottomSheet

--- presentation/src/main/java/com/msg/presentation/view/music/MusicScreen.kt
+++ presentation/src/main/java/com/msg/presentation/view/music/MusicScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,18 +19,22 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dotori.dotori_components.components.bottomsheet.DotoriBottomSheetDialog
 import com.dotori.dotori_components.components.button.DotoriButton
+import com.dotori.dotori_components.components.calendar.DotoriCalendar
 import com.dotori.dotori_components.components.dialog.DotoriDialog
 import com.dotori.dotori_components.components.music.DotoriMusicListItem
 import com.dotori.dotori_components.components.text_field.DotoriTextField
@@ -38,69 +43,96 @@ import com.dotori.dotori_components.theme.CalendarIcon
 import com.dotori.dotori_components.theme.DotoriText
 import com.dotori.dotori_components.theme.DotoriTheme
 import com.dotori.dotori_components.theme.PlusIcon
+import com.dotori.dotori_components.theme.ShortcutsIcon
+import com.dotori.dotori_components.theme.TrashCanIcon
 import com.dotori.dotori_components.theme.WarningIcon
 import com.dotori.dotori_components.theme.White
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
 @Composable
 fun MusicScreen(modifier: Modifier = Modifier) {
     var isDark by remember { mutableStateOf(false) }
     var showDialog by remember { mutableStateOf(false) }
+    var currentBottomSheetType by remember { mutableStateOf<BottomSheetType?>(null) }
 
-    if (showDialog) {
-        MusicDialog(onDismiss = { showDialog = false }) { }
-    }
+    val coroutineScope = rememberCoroutineScope()
 
-    CompositionLocalProvider(
-        LocalOverscrollConfiguration provides null
-    ) {
-        LazyColumn(modifier = modifier.fillMaxSize()) {
-            item {
-                DotoriTopBar {
-                    isDark = it
+    DotoriBottomSheetDialog(
+        sheetContent = {
+            currentBottomSheetType?.let { sheetType ->
+                when (sheetType) {
+                    BottomSheetType.Option -> MusicBottomSheetContent(
+                        onLinkClick = { /*TODO*/ },
+                        onDeleteClick = { /*TODO*/ }
+                    )
+
+                    BottomSheetType.Calendar -> DotoriCalendar { /*TODO*/ }
                 }
             }
-            stickyHeader {
-                MusicHeader(
-                    showMusicDialog = { showDialog = true }
-                )
-            }
-            item {
-                Divider(
-                    color = DotoriTheme.colors.background,
-                    thickness = 16.dp
-                )
-            }
-            val musicList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-            items(musicList) {
-                Box(
-                    modifier = Modifier
-                        .background(color = DotoriTheme.colors.background)
-                        .padding(horizontal = 20.dp)
-                ) {
-                    DotoriMusicListItem(
-                        modifier = Modifier
-                            .background(
-                                color = DotoriTheme.colors.cardBackground,
-                                shape = RoundedCornerShape(
-                                    topStart = if (musicList.first() == it) 16.dp else 0.dp,
-                                    topEnd = if (musicList.first() == it) 16.dp else 0.dp,
-                                    bottomStart = if (musicList.last() == it) 16.dp else 0.dp,
-                                    bottomEnd = if (musicList.last() == it) 16.dp else 0.dp
-                                )
-                            )
-                            .padding(horizontal = 16.dp)
-                            .padding(
-                                top = if (musicList.first() == it) 16.dp else 8.dp,
-                                bottom = if (musicList.last() == it) 16.dp else 8.dp
-                            ),
-                        imageUrl = "",
-                        title = "10cm- 서랍(그 해 우리는 OST Part.1)/가사 Audio Lyrics 21.12.07 New Release",
-                        name = "2105 김준",
-                        date = "16시 23분",
-                        onItemClicked = { /*TODO*/ }
-                    ) {
+        }
+    ) { sheetState ->
+        if (showDialog) {
+            MusicDialog(onDismiss = { showDialog = false }) { }
+        }
 
+        CompositionLocalProvider(
+            LocalOverscrollConfiguration provides null
+        ) {
+            LazyColumn(modifier = modifier.fillMaxSize()) {
+                item {
+                    DotoriTopBar {
+                        isDark = it
+                    }
+                }
+                stickyHeader {
+                    MusicHeader(
+                        showMusicDialog = { showDialog = true },
+                        showCalendar = {
+                            currentBottomSheetType = BottomSheetType.Calendar
+                            coroutineScope.launch { sheetState.show() }
+                        }
+                    )
+                }
+                item {
+                    Divider(
+                        color = DotoriTheme.colors.background,
+                        thickness = 16.dp
+                    )
+                }
+                val musicList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                items(musicList) {
+                    Box(
+                        modifier = Modifier
+                            .background(color = DotoriTheme.colors.background)
+                            .padding(horizontal = 20.dp)
+                    ) {
+                        DotoriMusicListItem(
+                            modifier = Modifier
+                                .background(
+                                    color = DotoriTheme.colors.cardBackground,
+                                    shape = RoundedCornerShape(
+                                        topStart = if (musicList.first() == it) 16.dp else 0.dp,
+                                        topEnd = if (musicList.first() == it) 16.dp else 0.dp,
+                                        bottomStart = if (musicList.last() == it) 16.dp else 0.dp,
+                                        bottomEnd = if (musicList.last() == it) 16.dp else 0.dp
+                                    )
+                                )
+                                .padding(horizontal = 16.dp)
+                                .padding(
+                                    top = if (musicList.first() == it) 16.dp else 8.dp,
+                                    bottom = if (musicList.last() == it) 16.dp else 8.dp
+                                ),
+                            imageUrl = "",
+                            title = "10cm- 서랍(그 해 우리는 OST Part.1)/가사 Audio Lyrics 21.12.07 New Release",
+                            name = "2105 김준",
+                            date = "16시 23분",
+                            onItemClicked = { /*TODO*/ },
+                            onOptionClicked = {
+                                currentBottomSheetType = BottomSheetType.Option
+                                coroutineScope.launch { sheetState.show() }
+                            }
+                        )
                     }
                 }
             }
@@ -128,7 +160,8 @@ fun DotoriTopBar(onSwitchClick: (Boolean) -> Unit) {
 @Composable
 fun MusicHeader(
     modifier: Modifier = Modifier,
-    showMusicDialog: () -> Unit
+    showMusicDialog: () -> Unit,
+    showCalendar: () -> Unit
 ) {
     Row(
         modifier = modifier
@@ -142,7 +175,10 @@ fun MusicHeader(
             color = DotoriTheme.colors.neutral10
         )
         Spacer(modifier = Modifier.weight(1f))
-        CalendarIcon(tint = DotoriTheme.colors.neutral20)
+        CalendarIcon(
+            modifier = Modifier.clickable { showCalendar() },
+            tint = DotoriTheme.colors.neutral20
+        )
         Spacer(modifier = Modifier.width(12.dp))
         PlusIcon(
             modifier = Modifier.clickable { showMusicDialog() },
@@ -178,7 +214,42 @@ fun MusicDialog(
             DotoriButton(
                 modifier = Modifier.fillMaxWidth(),
                 text = "신청하기",
+                paddingValues = PaddingValues(6.dp),
                 onClick = onButtonClick
+            )
+        }
+    }
+}
+
+@Composable
+fun MusicBottomSheetContent(
+    onLinkClick: () -> Unit,
+    onDeleteClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.padding(horizontal = 20.dp, vertical = 32.dp),
+        verticalArrangement = Arrangement.spacedBy(32.dp)
+    ) {
+        Row(
+            modifier = Modifier.clickable { onLinkClick() },
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ShortcutsIcon(tint = DotoriTheme.colors.neutral20)
+            Text(
+                text = "바로가기",
+                style = DotoriTheme.typography.body
+            )
+        }
+        Row(
+            modifier = Modifier.clickable { onDeleteClick() },
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            TrashCanIcon(tint = DotoriTheme.colors.neutral20)
+            Text(
+                text = "기상음악 삭제",
+                style = DotoriTheme.typography.body
             )
         }
     }
