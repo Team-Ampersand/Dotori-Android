50c5e41c1761accab4e75f03f5dad87d399e29ab
박성현 <pjhomg5530@naver.com>
2023-09-08 09:09:42 +0900

    :sparkles: :: mapper 생성

--- data/src/main/java/com/msg/data/remote/dto/auth/LoginResponse.kt
+++ data/src/main/java/com/msg/data/remote/dto/auth/LoginResponse.kt
@@ -1,8 +1,17 @@
 package com.msg.data.remote.dto.auth
 
+import com.msg.domain.model.auth.LoginResponseModel
+
 data class LoginResponse(
     val accessToken: String,
     val refreshToken: String,
     val expiresAt: String,
     val roles: Role
+)
+
+fun LoginResponse.asLoginResponseModel() = LoginResponseModel(
+    accessToken = accessToken,
+    refreshToken = refreshToken,
+    expiresAt = expiresAt,
+    roles = roles.asRoleModel()
 )
