dc1a72e5d16ae56ff936a579102e0d7e5f82c257
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-09-11 09:43:05 +0900

    :lipstick: :: 자습신청 screen dotori bottom sheet dialog 추가

--- presentation/src/main/java/com/msg/presentation/view/self_study/SelfStudyScreen.kt
+++ presentation/src/main/java/com/msg/presentation/view/self_study/SelfStudyScreen.kt
@@ -1,50 +1,94 @@
 package com.msg.presentation.view.self_study
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.dotori.dotori_components.components.bottomsheet.DotoriBottomSheetDialog
 import com.dotori.dotori_components.components.card.DotoriStudentCard
 import com.dotori.dotori_components.components.utils.Types
-import com.dotori.dotori_components.theme.DotoriTheme
-import com.dotori.dotori_components.theme.FilterIcon
+import com.msg.presentation.view.self_study.component.BottomSheetContent
+import com.msg.presentation.view.self_study.component.DotoriTopBar
+import com.msg.presentation.view.self_study.component.EmptySelfStudyScreen
+import com.msg.presentation.view.self_study.component.SelfStudyTopBar
+import kotlinx.coroutines.launch
 
 @Composable
+@OptIn(ExperimentalMaterialApi::class)
 fun SelfStudyScreen() {
+    val list = listOf(1,2,3,4,5)
+    val scope = rememberCoroutineScope()
+    var sheetCloseState by remember { mutableStateOf(false) }
+
+    DotoriBottomSheetDialog(
+        sheetContent = {
+            BottomSheetContent { searchText, grade, `class`, gender ->
+                sheetCloseState = true
+            }
+        }
+    ) { state ->
+        LaunchedEffect(sheetCloseState) {
+            state.hide()
+            sheetCloseState = false
+        }
+
+        if (list.isEmpty()) {
+            SelfStudyIsEmptyContent {
+                scope.launch { state.show() }
+            }
+        } else {
+            SelfStudyStudentListContent(
+                list = list,
+                onFilterIconClick = {
+                    scope.launch { state.show() }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun SelfStudyIsEmptyContent(onFilterIconClick: () -> Unit, ) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        DotoriTopBar(onSwitchClick = { /*TODO*/ })
+
+        SelfStudyTopBar { onFilterIconClick() }
+
+        EmptySelfStudyScreen()
+    }
+}
+
+@Composable
+@OptIn(ExperimentalFoundationApi::class)
+fun SelfStudyStudentListContent(
+    list: List<Int>,
+    onFilterIconClick: () -> Unit
+) {
     var checkBoxState by remember { mutableStateOf(false) }
 
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
     ) {
-        val list = listOf(1,2,3,4,5)
+        item { DotoriTopBar(onSwitchClick = { /*TODO*/ }) }
 
-        item {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = "자습신청",
-                        style = DotoriTheme.typography.subTitle1,
-                        color = DotoriTheme.colors.neutral10
-                    )
-                },
-                backgroundColor = DotoriTheme.colors.cardBackground,
-                actions = { FilterIcon() }
-            )
-        }
+        stickyHeader { SelfStudyTopBar { onFilterIconClick() } }
 
         items(list) { position ->
             DotoriStudentCard(
                 name = "임가람",
                 gender = "MAN",
-                role = "ROLE_STUDENT",
+                role = "ROLE_MEMBER",
                 studentNumber = "3412",
                 position = position,
                 mode = Types.CardType.SELF_STUDY,
