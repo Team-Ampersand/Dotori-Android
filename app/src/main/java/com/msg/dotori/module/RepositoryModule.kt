b796ac95cae896f62963050da9fce32fe3cdaff4
hyeonseo <s21035@gsm.hs.kr>
2023-09-13 17:53:35 +0900

    :sparkles: :: MusicRepository DI

--- app/src/main/java/com/msg/dotori/module/RepositoryModule.kt
+++ app/src/main/java/com/msg/dotori/module/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.msg.dotori.module
 
 import com.msg.data.repository.AuthRepositoryImpl
+import com.msg.data.repository.MusicRepositoryImpl
 import com.msg.domain.repository.AuthRepository
+import com.msg.domain.repository.MusicRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -12,4 +14,7 @@ import dagger.hilt.components.SingletonComponent
 interface RepositoryModule {
     @Binds
     fun bindsAuthRepository(authRepositoryImpl: AuthRepositoryImpl): AuthRepository
+
+    @Binds
+    fun bindsMusicRepository(musicRepositoryImpl: MusicRepositoryImpl): MusicRepository
 }
