a095acb4260891285696bd9aa47d81e9328cf594
박성현 <pjhomg5530@naver.com>
2023-09-15 08:53:45 +0900

    :recycle: :: 줄 개행

--- presentation/src/main/java/com/msg/presentation/view/student_info/component/StudentInfoBottomSheetType.kt
+++ presentation/src/main/java/com/msg/presentation/view/student_info/component/StudentInfoBottomSheetType.kt
@@ -1,5 +1,7 @@
 package com.msg.presentation.view.student_info.component
 
 enum class StudentInfoBottomSheetType {
-    StudentInfo, Filter, ModifyStudentInfo
+    StudentInfo,
+    Filter,
+    ModifyStudentInfo
 }
