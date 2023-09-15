3607678b9a8443ad55f36f385f78e6e6f34859e8
박성현 <pjhomg5530@naver.com>
2023-09-15 15:16:32 +0900

    :sparkles: :: datasource 생성

---
+++ data/src/main/java/com/msg/data/remote/datasource/student_info/StudentInfoDataSource.kt
@@ -0,0 +1,20 @@
+package com.msg.data.remote.datasource.student_info
+
+import com.msg.data.remote.dto.student_info.SearchStudentInfoResponse
+import com.msg.data.remote.dto.student_info.StudentInfoRequest
+import com.msg.data.remote.dto.student_info.StudentInfoResponse
+
+interface StudentInfoDataSource {
+    suspend fun getAllStudentInfo(): List<StudentInfoResponse>
+
+    suspend fun getSearchStudentInfo(
+        name: String,
+        gender: String,
+        classNum: String,
+        grade: Long,
+        role: String,
+        selfStudyCheck: Boolean,
+    ): List<SearchStudentInfoResponse>
+
+    suspend fun modifyStudentInfo(body: StudentInfoRequest)
+}
---
+++ data/src/main/java/com/msg/data/remote/datasource/student_info/StudentInfoDataSourceImpl.kt
@@ -0,0 +1,38 @@
+package com.msg.data.remote.datasource.student_info
+
+import com.msg.data.remote.dto.student_info.SearchStudentInfoResponse
+import com.msg.data.remote.dto.student_info.StudentInfoRequest
+import com.msg.data.remote.dto.student_info.StudentInfoResponse
+import com.msg.data.remote.network.StudentInfoApi
+import com.msg.data.remote.util.safeApiCall
+import javax.inject.Inject
+
+class StudentInfoDataSourceImpl @Inject constructor(
+    private val studentInfoApi: StudentInfoApi
+): StudentInfoDataSource {
+    override suspend fun getAllStudentInfo(): List<StudentInfoResponse> = safeApiCall {
+        studentInfoApi.getAllStudentInfo()
+    }
+
+    override suspend fun getSearchStudentInfo(
+        name: String,
+        gender: String,
+        classNum: String,
+        grade: Long,
+        role: String,
+        selfStudyCheck: Boolean
+    ): List<SearchStudentInfoResponse> = safeApiCall {
+        studentInfoApi.getSearchStudentInfo(
+            name =  name,
+            gender =  gender,
+            classNum =classNum,
+            grade = grade,
+            role = role,
+            selfStudyCheck = selfStudyCheck
+        )
+    }
+
+    override suspend fun modifyStudentInfo(body: StudentInfoRequest) = safeApiCall {
+        studentInfoApi.modifyStudentInfo(body)
+    }
+}
