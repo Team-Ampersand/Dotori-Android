f3c8640c6ee8710926c23a5aa433a36b10810278
박성현 <pjhomg5530@naver.com>
2023-09-16 10:10:31 +0900

    :fire: :: 불 필요한 Depth 지우기

--- data/src/main/java/com/msg/data/remote/dto/auth/Role.kt
+++ data/src/main/java/com/msg/data/remote/dto/auth/Role.kt
@@ -10,11 +10,9 @@ enum class Role {
     ROLE_DEVELOPER
 }
 
-fun Role.asRoleModel(): RoleModel {
-    return when (this) {
-        Role.ROLE_ADMIN -> RoleModel.ROLE_ADMIN
-        Role.ROLE_MEMBER -> RoleModel.ROLE_MEMBER
-        Role.ROLE_COUNCILLOR -> RoleModel.ROLE_COUNCILLOR
-        Role.ROLE_DEVELOPER -> RoleModel.ROLE_DEVELOPER
-    }
+fun Role.asRoleModel() = when (this) {
+    Role.ROLE_ADMIN -> RoleModel.ROLE_ADMIN
+    Role.ROLE_MEMBER -> RoleModel.ROLE_MEMBER
+    Role.ROLE_COUNCILLOR -> RoleModel.ROLE_COUNCILLOR
+    Role.ROLE_DEVELOPER -> RoleModel.ROLE_DEVELOPER
 }
