80faa4efebe9e30be489c3f493e36489544861c0
박성현 <pjhomg5530@naver.com>
2023-09-18 14:45:26 +0900

    :sparkles: :: DotoriNavHost 적용

--- app/src/main/java/com/msg/dotori/MainActivity.kt
+++ app/src/main/java/com/msg/dotori/MainActivity.kt
@@ -2,12 +2,16 @@ package com.msg.dotori
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContent {
+            DotoriNavHost(navController = rememberNavController(), startDestination = "loginRoute")
+        }
     }
 }
