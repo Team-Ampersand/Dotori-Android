d5f84039b65b042c6ddecb2299e022c2c8a2fb47
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-09-18 10:58:30 +0900

    :sparkles: :: mapper 생성

--- data/src/main/java/com/msg/data/remote/dto/self_study/SelfStudyInfoResponse.kt
+++ data/src/main/java/com/msg/data/remote/dto/self_study/SelfStudyInfoResponse.kt
@@ -1,7 +1,15 @@
 package com.msg.data.remote.dto.self_study
 
+import com.msg.domain.model.self_study.SelfStudyInfoResponseModel
+
 data class SelfStudyInfoResponse(
     val count: Int,
     val limit: Int,
-    val selfStudyStatus: String,
+    val selfStudyStatus: String
+)
+
+fun SelfStudyInfoResponse.asSelfStudyInfoResponseModel() = SelfStudyInfoResponseModel(
+    count = count,
+    limit = limit,
+    selfStudyStatus = selfStudyStatus
 )
--- data/src/main/java/com/msg/data/remote/dto/self_study/SelfStudyListResponse.kt
+++ data/src/main/java/com/msg/data/remote/dto/self_study/SelfStudyListResponse.kt
@@ -1,5 +1,7 @@
 package com.msg.data.remote.dto.self_study
 
+import com.msg.domain.model.self_study.SelfStudyListResponseModel
+
 data class SelfStudyListResponse(
     val rank: Int,
     val id: Int,
@@ -7,4 +9,13 @@ data class SelfStudyListResponse(
     val memberName: String,
     val gender: String,
     val selfStudyCheck: Boolean
+)
+
+fun SelfStudyListResponse.asSelfStudyListResponseModel() = SelfStudyListResponseModel(
+    rank = rank,
+    id = id,
+    stuNum = stuNum,
+    memberName = memberName,
+    gender = gender,
+    selfStudyCheck = selfStudyCheck
 )
