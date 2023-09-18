959fc7fd9b8a2b150a046cd5d6dde41db201ffd9
hyeonseo <s21035@gsm.hs.kr>
2023-09-18 20:19:10 +0900

    :sparkles: :: `getYoutubeMusic()` 구현

--- data/src/main/java/com/msg/data/remote/network/MusicApi.kt
+++ data/src/main/java/com/msg/data/remote/network/MusicApi.kt
@@ -1,13 +1,16 @@
 package com.msg.data.remote.network
 
-import com.msg.data.remote.dto.music.MusicRequest
-import com.msg.data.remote.dto.music.MusicResponse
+import com.msg.data.BuildConfig
+import com.msg.data.remote.dto.music.request.MusicRequest
+import com.msg.data.remote.dto.music.response.MusicResponse
+import com.msg.data.remote.dto.music.response.YoutubeResponse
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
+import retrofit2.http.Url
 
 interface MusicApi {
     @GET("/{role}/music")
@@ -27,4 +30,11 @@ interface MusicApi {
         @Path("role") role: String,
         @Path("music_id") musicId: Long
     )
+
+    @GET
+    suspend fun getYoutubeMusic(
+        @Url oembedUrl: String = BuildConfig.OEMBED_URL,
+        @Query("url") youtubeUrl: String,
+        @Query("format") format: String = "json"
+    ): YoutubeResponse
 }
