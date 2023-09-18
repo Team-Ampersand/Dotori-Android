033ead41c445d87566eb757280e4255375c7848d
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-09-18 10:59:02 +0900

    :sparkles: :: self study repository 생성

--- data/src/main/java/com/msg/data/repository/SelfStudyRepositoryImpl.kt
+++ data/src/main/java/com/msg/data/repository/SelfStudyRepositoryImpl.kt
@@ -1,12 +1,36 @@
 package com.msg.data.repository
 
 import com.msg.data.remote.datasource.self_study.SelfStudyDataSource
+import com.msg.data.remote.dto.self_study.asSelfStudyInfoResponseModel
+import com.msg.data.remote.dto.self_study.asSelfStudyListResponseModel
+import com.msg.domain.model.self_study.SelfStudyInfoResponseModel
+import com.msg.domain.model.self_study.SelfStudyListResponseModel
 import com.msg.domain.repository.SelfStudyRepository
 import javax.inject.Inject
 
 class SelfStudyRepositoryImpl @Inject constructor(
     private val selfStudyDataSource: SelfStudyDataSource
 ): SelfStudyRepository {
+    override suspend fun getSelfStudyInfo(role: String): SelfStudyInfoResponseModel =
+        selfStudyDataSource.getSelfStudyInfo(role = role).asSelfStudyInfoResponseModel()
+
+    override suspend fun getSelfStudyList(role: String): List<SelfStudyListResponseModel> =
+        selfStudyDataSource.getSelfStudyList(role = role).map { it.asSelfStudyListResponseModel() }
+
+    override suspend fun searchSelfStudyStudent(
+        role: String,
+        name: String?,
+        gender: String?,
+        classNum: String?,
+        grade: Int?
+    ): List<SelfStudyListResponseModel> = selfStudyDataSource.searchSelfStudyStudent(
+        role = role,
+        name = name,
+        gender = gender,
+        classNum = classNum,
+        grade = grade
+    ).map { it.asSelfStudyListResponseModel() }
+
     override suspend fun banSelfStudy(role: String, userId: Long) =
         selfStudyDataSource.banSelfStudy(
             role = role,
@@ -17,5 +41,24 @@ class SelfStudyRepositoryImpl @Inject constructor(
         selfStudyDataSource.banCancelSelfStudy(
             role = role,
             userId = userId
+        )
+
+    override suspend fun selfStudy(role: String) =
+        selfStudyDataSource.selfStudy(role)
+
+    override suspend fun cancelSelfStudy(role: String) =
+        selfStudyDataSource.cancelSelfStudy(role)
+
+    override suspend fun changeSelfStudyLimit(role: String, limit: Int) =
+        selfStudyDataSource.changeSelfStudyLimit(
+            role = role,
+            limit = limit
+        )
+
+    override suspend fun checkSelfStudy(role: String, memberId: Long, selfStudyCheck: Boolean) =
+        selfStudyDataSource.checkSelfStudy(
+            role = role,
+            memberId = memberId,
+            selfStudyCheck = selfStudyCheck
         )
 }
