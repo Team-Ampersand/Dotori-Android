cdec12736899ebf63076eef7af1eaed2d03fc436
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-09-18 09:11:18 +0900

    :sparkles: :: 자습 신청 data source 함수 추가

--- data/src/main/java/com/msg/data/remote/datasource/self_study/SelfStudyDataSource.kt
+++ data/src/main/java/com/msg/data/remote/datasource/self_study/SelfStudyDataSource.kt
@@ -1,6 +1,25 @@
 package com.msg.data.remote.datasource.self_study
 
+import com.msg.data.remote.dto.self_study.SelfStudyInfoResponse
+import com.msg.data.remote.dto.self_study.SelfStudyListResponse
+
 interface SelfStudyDataSource {
+    suspend fun selfStudyInfo(
+        role: String
+    ): SelfStudyInfoResponse
+
+    suspend fun selfStudyList(
+        role: String
+    ): List<SelfStudyListResponse>
+
+    suspend fun searchSelfStudyStudent(
+        role: String,
+        name: String?,
+        gender: String?,
+        classNum: String?,
+        grade: Int?
+    ): List<SelfStudyListResponse>
+
     suspend fun banSelfStudy(
         role: String,
         userId: Long
@@ -9,5 +28,20 @@ interface SelfStudyDataSource {
     suspend fun banCancelSelfStudy(
         role: String,
         userId: Long
+    )
+
+    suspend fun selfStudy(role: String)
+
+    suspend fun cancelSelfStudy(role: String)
+
+    suspend fun changeSelfStudyLimit(
+        role: String,
+        limit: Int
+    )
+
+    suspend fun checkSelfStudy(
+        role: String,
+        memberId: String,
+        selfStudyCheck: Boolean
     )
 }
