41f2661725b133919a78fc914b22b3aa70a691b4
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-09-19 17:23:26 +0900

    :sparkles: :: domain response 생성

---
+++ domain/src/main/java/com/msg/domain/model/massage/MassageListResponseModel.kt
@@ -0,0 +1,9 @@
+package com.msg.domain.model.massage
+
+data class MassageListResponseModel(
+    val rank: Int,
+    val id: Int,
+    val stuNum: String,
+    val memberName: String,
+    val gender: String
+)
