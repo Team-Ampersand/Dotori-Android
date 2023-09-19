cb0ee680ca96231d325cf99bbe0a97fa3485a509
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-09-19 17:25:03 +0900

    :sparkles: :: massage mapper 생성

--- data/src/main/java/com/msg/data/remote/dto/massage/MassageInfoResponse.kt
+++ data/src/main/java/com/msg/data/remote/dto/massage/MassageInfoResponse.kt
@@ -1,7 +1,15 @@
 package com.msg.data.remote.dto.massage
 
+import com.msg.domain.model.massage.MassageInfoResponseModel
+
 data class MassageInfoResponse(
     val count: Int,
     val limit: Int,
     val massageStatus: String
+)
+
+fun MassageInfoResponse.asMassageInfoResponseModel() = MassageInfoResponseModel(
+    count = count,
+    limit = limit,
+    massageStatus = massageStatus
 )
--- data/src/main/java/com/msg/data/remote/dto/massage/MassageListResponse.kt
+++ data/src/main/java/com/msg/data/remote/dto/massage/MassageListResponse.kt
@@ -1,9 +1,19 @@
 package com.msg.data.remote.dto.massage
 
+import com.msg.domain.model.massage.MassageListResponseModel
+
 data class MassageListResponse(
     val rank: Int,
     val id: Int,
     val stuNum: String,
     val memberName: String,
     val gender: String
+)
+
+fun MassageListResponse.asMassageListResponseModel() = MassageListResponseModel(
+    rank = rank,
+    id = id,
+    stuNum = stuNum,
+    memberName = memberName,
+    gender = gender
 )
