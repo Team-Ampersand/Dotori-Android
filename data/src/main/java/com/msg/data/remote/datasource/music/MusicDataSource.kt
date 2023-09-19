b1044f0a7b32287b2fd897d90634b5f104ab4375
hyeonseo <s21035@gsm.hs.kr>
2023-09-19 09:19:08 +0900

    :sparkles: :: MusicDataSource

--- data/src/main/java/com/msg/data/remote/datasource/music/MusicDataSource.kt
+++ data/src/main/java/com/msg/data/remote/datasource/music/MusicDataSource.kt
@@ -2,6 +2,7 @@ package com.msg.data.remote.datasource.music
 
 import com.msg.data.remote.dto.music.request.MusicRequest
 import com.msg.data.remote.dto.music.response.MusicResponse
+import com.msg.data.remote.dto.music.response.YoutubeResponse
 
 interface MusicDataSource {
     suspend fun getMusics(role: String, date: String): List<MusicResponse>
@@ -9,4 +10,6 @@ interface MusicDataSource {
     suspend fun requestMusic(role: String, body: MusicRequest)
 
     suspend fun deleteMusic(role: String, musicId: Long)
+
+    suspend fun getYoutubeMusic(youtubeUrl: String): YoutubeResponse
 }
