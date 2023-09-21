20a7898877ff7f3db0d63ff028501274a49246d4
hyeonseo <s21035@gsm.hs.kr>
2023-09-21 17:01:19 +0900

    :sparkles: :: 잘못 주석 처리 된 부분 수정

--- presentation/src/main/java/com/msg/presentation/view/notice/component/NoticeEditImage.kt
+++ presentation/src/main/java/com/msg/presentation/view/notice/component/NoticeEditImage.kt
@@ -1,5 +1,8 @@
 package com.msg.presentation.view.notice.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,7 +11,9 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.dotori.dotori_components.theme.DotoriTheme
 import com.dotori.dotori_components.theme.ExclamationMarkIcon
@@ -34,15 +39,15 @@ fun NoticeEditImage(
         }
         LazyRow {
             item {
-//                Image(
-//                    modifier = Modifier.clickable(
-//                        interactionSource = remember { MutableInteractionSource() },
-//                        indication = null,
-//                        onClick = { /* TODO: 갤러리에서 이미지 불러오는 로직 */ }
-//                    ),
-//                    painter = painterResource(R.drawable.ic_add_image),
-//                    contentDescription = "이미지 추가"
-//                )
+                Image(
+                    modifier = Modifier.clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null,
+                        onClick = { /* TODO: 갤러리에서 이미지 불러오는 로직 */ }
+                    ),
+                    painter = painterResource(R.drawable.ic_add_image),
+                    contentDescription = "이미지 추가"
+                )
             }
             items(images) {
                 GlideImage(
