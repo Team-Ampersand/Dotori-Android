2b165505abb0afedcc73a16c2a2765616741d3dc
박성현 <pjhomg5530@naver.com>
2023-09-21 11:54:47 +0900

    :recycle: :: 규정 위반(item)을 눌렀을 때 해당하는 상세 규정 위반 리스트 보여주기

--- presentation/src/main/java/com/msg/presentation/view/rule_violation/component/RuleViolationDialogContent.kt
+++ presentation/src/main/java/com/msg/presentation/view/rule_violation/component/RuleViolationDialogContent.kt
@@ -125,12 +125,62 @@ fun RuleViolationCheckDialogContent(onDismiss: () -> Unit) {
         "외부인 출입 관여"
     )
     val detailRuleViolationList = listOf(
-        "• 반입 - 화기류",
-        "• 반입 - 흉기",
-        "• 반입 - 주류",
-        "• 반입 - 담배",
-        "• 반입 - 사행성기구",
-        "• 반입 - 음식"
+        listOf( // 금지 물품 반입
+            "• 반입 - 화기류",
+            "• 반입 - 흉기",
+            "• 반입 - 주류",
+            "• 반입 - 담배",
+            "• 반입 - 사행성기구",
+            "• 반입 - 음식"
+        ),
+        listOf( // 금지 물품 사용
+            "• 사용 - 화기류",
+            "• 사용 - 흉기",
+            "• 사용 - 주류",
+            "• 사용 - 담배",
+            "• 사용 - 사행성기구",
+            "• 사용 - 음식",
+        ),
+        listOf( // 사감 지도 불이행
+            "• 사감의 학습 및 생활지도 불이행"
+        ),
+        listOf( // 시간 관리 소홀 및 이탈 행위
+            "• 지각",
+            "• 외출",
+            "• 외박"
+        ),
+        listOf( // 물품 훼손 및 절도
+            "• 물품 훼손",
+            "• 절도",
+            "• 갈취",
+        ),
+        listOf( // 취침 방해
+            "• 타호실 출입",
+            "• 전자기기 소지",
+            "• 고성방가",
+        ),
+        listOf( // 공동 생활 방해 및 위생 상태 불량
+            "• 지정시간 외 기숙사 출입",
+            "• 세탁기,건조기에 세탁물 방치",
+            "• 공공시설물 사용규정 위반",
+            "• 부착된 게시물 훼손 및 낙서",
+            "• 전자기기 소지 혹은 사용",
+            "• 호실 정리정돈 상태 불량",
+            "• 기숙사 환경 오염"
+        ),
+        listOf( // 애정 행위
+            "• 애정 행위",
+            "• 성적 행위"
+        ),
+        listOf( // 기숙사 출입 금지 구역 출입
+            "• 출입금지 구역 출입"
+        ),
+        listOf( // 학습실 면학분위기 저해
+            "• 학습실 면학분위기 저해"
+        ),
+        listOf( // 외부인 출입 관여
+            "• 외부인 출입 허가"
+        )
     )
 
     Column {
@@ -177,7 +227,7 @@ fun RuleViolationCheckDialogContent(onDismiss: () -> Unit) {
                     ),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                detailRuleViolationList.forEach {
+                detailRuleViolationList[isDetailClicked].forEach {
                     Text(
                         text = it,
                         style = DotoriTheme.typography.smallTitle,
@@ -366,5 +416,5 @@ fun RuleViolationDeleteDialogContent(
 @Preview
 @Composable
 fun RuleViolationListDialogPreview() {
-    RuleViolationDialogContent(RuleViolationDialogType.CREATE) {}
+    RuleViolationDialogContent(RuleViolationDialogType.CHECK) {}
 }
