27d12d91c9583fc40360cac273ad8b5b5abcba61
hyeonseo <s21035@gsm.hs.kr>
2023-09-25 11:48:44 +0900

    :sparkles: :: NoticeEditViewModel 로직 연결

--- presentation/src/main/java/com/msg/presentation/view/notice/NoticeEditScreen.kt
+++ presentation/src/main/java/com/msg/presentation/view/notice/NoticeEditScreen.kt
@@ -21,6 +21,8 @@ import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,24 +30,42 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.dotori.dotori_components.components.dialog.DotoriDialog
 import com.dotori.dotori_components.components.text_field.DotoriTextField
 import com.dotori.dotori_components.theme.DotoriTheme
 import com.dotori.dotori_components.theme.White
 import com.dotori.dotori_components.theme.XMarkIcon
+import com.msg.domain.model.notice.request.NoticeRequestModel
 import com.msg.presentation.view.music.component.DotoriTopBar
 import com.msg.presentation.view.notice.component.NoticeDialogContent
 import com.msg.presentation.view.notice.component.NoticeEditHeader
 import com.msg.presentation.view.notice.component.NoticeEditImage
 import com.msg.presentation.view.util.updateDotoriTheme
+import com.msg.presentation.viewmodel.notice.NoticeEditViewModel
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun NoticeEditScreen() {
-    var title by remember { mutableStateOf("") }
-    var content by remember { mutableStateOf("") }
+fun NoticeEditScreen(noticeEditViewModel: NoticeEditViewModel = hiltViewModel()) {
+    val noticeId: Long? = 1 /* TODO: NavArg에 선택적으로 받도록 설정 */
+    val roleUiState by noticeEditViewModel.roleUiState.collectAsState()
+    val noticeUiState by noticeEditViewModel.noticeUiState.collectAsState()
+
+    LaunchedEffect(Unit) {
+        noticeEditViewModel.getRole()
+
+        if (noticeId != null) {
+            noticeEditViewModel.getNoticeDetail(
+                role = roleUiState.data!!,
+                noticeId = noticeId
+            )
+        }
+    }
+
+    var title by remember { mutableStateOf(noticeUiState.data?.title ?: "") }
+    var content by remember { mutableStateOf(noticeUiState.data?.content ?: "") }
     var showDialog by remember { mutableStateOf(false) }
 
     if (showDialog) {
@@ -78,10 +98,29 @@ fun NoticeEditScreen() {
                     color = DotoriTheme.colors.neutral40
                 )
             }
+            val role = roleUiState.data!!
             stickyHeader {
                 NoticeEditHeader(
                     onBackClick = { showDialog = true },
-                    onSaveClick = { /* TODO: 공지 저장 이벤트 처리 */ }
+                    onSaveClick = {
+                        val noticeRequestModel = NoticeRequestModel(
+                            title = title,
+                            content = content
+                        )
+                        if (noticeId != null) {
+                            noticeEditViewModel.modifyNotice(
+                                role = role,
+                                noticeId = noticeId,
+                                body = noticeRequestModel
+                            )
+                        } else {
+                            noticeEditViewModel.writeNotice(
+                                role = role,
+                                files = emptyList(),
+                                noticeRequestModel = noticeRequestModel
+                            )
+                        }
+                    }
                 )
             }
             val createdDate = LocalDateTime.now()
@@ -92,12 +131,19 @@ fun NoticeEditScreen() {
                         .fillMaxWidth()
                         .padding(horizontal = 20.dp)
                 ) {
+                    val author = when (role) {
+                        "ROLE_DEVELOPER" -> "도토리"
+                        "ROLE_COUNCILLOR" -> "기숙사자치위원회"
+                        "ROLE_ADMIN" -> "사감선생님"
+                        else -> "학생"
+                    }
+
                     Row(
                         modifier = Modifier.fillMaxWidth(),
                         horizontalArrangement = Arrangement.SpaceBetween
                     ) {
                         Text(
-                            text = "작성자: 도토리",
+                            text = "작성자: $author",
                             style = DotoriTheme.typography.subTitle1,
                             color = DotoriTheme.colors.neutral10
                         )
@@ -136,11 +182,10 @@ fun NoticeEditScreen() {
                     Spacer(modifier = Modifier.height(32.dp))
                 }
             }
-            val images = listOf("", "", "")
             item {
                 NoticeEditImage(
                     modifier = Modifier.padding(horizontal = 20.dp),
-                    images = images
+                    images = noticeUiState.data?.noticeImages ?: emptyList()
                 )
             }
         }
