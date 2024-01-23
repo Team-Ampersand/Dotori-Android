4b430d5023ccfe722bec37b3b2f91b66c2acbb8c
yeongun130 <yeongunj625@gmail.com>
2024-01-23 20:57:08 +0900

    :recycle: :: end point 수정

--- data/src/main/java/com/msg/data/remote/network/RuleViolationApi.kt
+++ data/src/main/java/com/msg/data/remote/network/RuleViolationApi.kt
@@ -11,18 +11,18 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RuleViolationApi {
-    @GET("/{role}/rule/{stu_num}")
+    @GET("{role}/rule/{stu_num}")
     suspend fun getDetailRuleViolation(
         @Path("role") role: String,
         @Path("stu_num") stuNum: String
     ): List<RuleViolationResponse>
 
-    @GET("/{role}/rule")
+    @GET("{role}/rule")
     suspend fun getSelfRuleViolation(
         @Path("role") role: String
     ): List<RuleViolationResponse>
 
-    @GET("/{role}/rule")
+    @GET("{role}/rule")
     suspend fun searchRuleViolation(
         @Path("role") role: String,
         @Query("memberName") memberName: String,
@@ -30,13 +30,13 @@ interface RuleViolationApi {
         @Query("gender") gender: String
     ): List<SearchRuleViolationResponse>
 
-    @POST("/{role}/rule")
+    @POST("{role}/rule")
     suspend fun postRuleViolation(
         @Path("role") role: String,
         @Body ruleViolationRequest: RuleViolationRequest
     )
 
-    @DELETE("/{role}/rule/{rule_id}")
+    @DELETE("{role}/rule/{rule_id}")
     suspend fun deleteRuleViolation(
         @Path("role") role: String,
         @Path("rule_id") ruleId: String
