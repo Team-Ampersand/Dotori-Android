21b3cf09eb16dc74a9e9b7ad97a34097e330eca1
yeongun130 <yeongunj625@gmail.com>
2024-01-23 21:00:25 +0900

    :memo: :: ViewModel 어노테이 추가

--- presentation/src/main/java/com/msg/presentation/viewmodel/LoginViewModel.kt
+++ presentation/src/main/java/com/msg/presentation/viewmodel/LoginViewModel.kt
@@ -1,5 +1,6 @@
 package com.msg.presentation.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.msg.domain.model.auth.LoginRequestModel
@@ -9,11 +10,13 @@ import com.msg.domain.usecase.auth.SaveRoleUseCase
 import com.msg.domain.usecase.auth.SaveTokenUseCase
 import com.msg.presentation.exception.exceptionHandling
 import com.msg.presentation.viewmodel.util.Event
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class LoginViewModel @Inject constructor(
     private val loginUseCase: LoginUseCase,
     private val saveTokenUseCase: SaveTokenUseCase,
@@ -33,8 +36,10 @@ class LoginViewModel @Inject constructor(
                         expiresAt = it.expiresAt
                     )
                     saveRoleUseCase(it.roles.toString())
+                    Log.d("failure", "success")
                 }
                 .onFailure {
+                    Log.d("failure", it.message.toString())
                     it.exceptionHandling(
                         badRequestAction = { _loginState.value = Event.BadRequest },
                         notFoundAction = { _loginState.value = Event.NotFound }
