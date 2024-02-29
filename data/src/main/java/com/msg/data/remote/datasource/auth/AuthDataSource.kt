f635e988a9df88a6ae520e7b04b63954e13e149b
송현서 <s22026@gsm.hs.kr>
2024-02-29 16:40:06 +0900

    :sparkles: :: Add change password to AuthDataSource

--- data/src/main/java/com/msg/data/remote/datasource/auth/AuthDataSource.kt
+++ data/src/main/java/com/msg/data/remote/datasource/auth/AuthDataSource.kt
@@ -1,5 +1,6 @@
 package com.msg.data.remote.datasource.auth
 
+import com.msg.data.remote.dto.auth.ChangePasswordRequest
 import com.msg.data.remote.dto.auth.LoginRequest
 import com.msg.data.remote.dto.auth.LoginResponse
 import com.msg.data.remote.dto.auth.SignUpRequest
@@ -10,4 +11,6 @@ interface AuthDataSource {
     suspend fun tokenReissue(): LoginResponse
 
     suspend fun signUp(body: SignUpRequest)
+
+    suspend fun changePassword(body: ChangePasswordRequest)
 }
